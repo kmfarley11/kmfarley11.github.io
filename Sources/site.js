7a31ed2840a03695855a8b5fa288d35eb9aaa61b
farleykm <farleykm@mail.uc.edu>
2015-06-30 23:26:29 -0400

    mess with animations & css, add my face

--- Sources/site.js
+++ Sources/site.js
@@ -1,6 +1,15 @@
-/* behaviour file */
-/*
-$(document).ready(function () {
-	$('.active-link').attr('bakcgound-color', '#fff'); //Disable
-});
-*/
+// The controller is a regular JavaScript function. It is called
+// once when AngularJS runs into the ng-controller declaration.
+
+function EPorfolioController($scope){
+	
+	$scope.showtooltip = false;
+	$scope.value = 'Edit me.';
+	$scope.selected = 'home';
+	
+	$scope.newSelection = function(e){
+		var selection = e.originalEvent.currentTarget.attributes.link;
+		if(selection)
+			$scope.selected = selection.value;
+	};
+}
